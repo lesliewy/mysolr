e95fbb5266f62a7670e33ca5502cbabb225eb26c
lesliewy <leslie_wangyang@163.com>
2016-12-05 21:07:12 +0800

    change

--- SolrMongoImporter/src/main/org/apache/solr/handler/dataimport/MongoMapperTransformer.java
+++ SolrMongoImporter/src/main/org/apache/solr/handler/dataimport/MongoMapperTransformer.java
@@ -22,52 +22,12 @@ public class MongoMapperTransformer extends Transformer {
             String mongoFieldName = map.get(MONGO_FIELD);
             if (mongoFieldName == null)
                 continue;
-
             String columnFieldName = map.get(DataImporter.COLUMN);
-            /*  对于.分隔的字段认为是嵌套的, 这里不考虑通用性了 */
-            /*
-            String[] phrases = mongoFieldName.split("\\.");
-            Author author = null;
-            Category category = null;
-            List<Poem> poems = null;
-            if(phrases.length > 1){
-            	if("author".equalsIgnoreCase(phrases[0])){
-            		author = GsonUtil.parseJsonWithGson(row.get(phrases[0]).toString(), Author.class);
-            		if("name".equalsIgnoreCase(phrases[1])){
-            			row.put(columnFieldName, author.getName());
-            		}else if("url".equalsIgnoreCase(phrases[1])){
-            			row.put(columnFieldName, author.getUrl());
-            		}else if("numofpoems".equalsIgnoreCase(phrases[1])){
-            			row.put(columnFieldName, author.getNumofpoems());
-            		}else if("brief".equalsIgnoreCase(phrases[1])){
-            			row.put(columnFieldName, author.getBrief());
-            		}
-            	}else if("category".equalsIgnoreCase(phrases[0])){
-            		category = GsonUtil.parseJsonWithGson(row.get(phrases[0]).toString(), Category.class);
-            		if("name".equalsIgnoreCase(phrases[1])){
-            			row.put(columnFieldName, category.getName());
-            		}else if("url".equalsIgnoreCase(phrases[1])){
-            			row.put(columnFieldName, category.getUrl());
-            		}else if("numofauthors".equalsIgnoreCase(phrases[1])){
-            			row.put(columnFieldName, category.getNumofauthors());
-            		}
-            	}
-            }else if("poems".equalsIgnoreCase(phrases[0])){
-            	LOG.info("leslie poemsa" + row.get(phrases[0]).toString());
-            	poems = GsonUtil.parsePoemsWithGson(row.get(phrases[0]).toString());
-            	int size = poems.size();
-            	LOG.info("leslie size: " + size);
-            	for(int i = 0; i < size; i++){
-            		Poem poem = poems.get(i);
-            		row.put("poem." + i + ".name", poem.getName());
-            		row.put("poem." + i + ".url", poem.getUrl());
-            		row.put("poem." + i + ".content", poem.getContent());
-            		row.put("poem." + i + ".appreciation", poem.getAppreciation());
-            	}
-            }else{
-               row.put(columnFieldName, row.get(mongoFieldName));
+            /*  unwind的结果中同一个数组的记录的_id都是一样的，需要做下处理. */
+            if("_id".equalsIgnoreCase(mongoFieldName)){
+            	row.put(columnFieldName, String.valueOf(System.nanoTime()));
+            	continue;
             }
-            */
             row.put(columnFieldName, row.get(mongoFieldName));
         }
         /*
